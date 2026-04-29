c66832e3d2ec9c3a453c133b33b3cfe76bd85dcf
monukumar98 <monu012as@gmail.com>
2026-04-29 21:43:00 +0530

    Mirror and Number Pattern

---
+++ Lec3/Pattern14.java
@@ -0,0 +1,38 @@
+package Lec3;
+
+public class Pattern14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int row = 1;
+		int star = 1;
+		int space = n - 1;
+		while (row <= 2 * n - 1) {
+			// space
+			int i = 1;
+			while (i <= space) {
+				System.out.print("  ");
+				i++;
+
+			}
+			int j = 1;
+			while (j <= star) {
+				System.out.print("* ");
+				j++;
+			}
+			// Mirror
+			if (row < n) {
+				star++;
+				space--;
+			} else {
+				star--;
+				space++;
+			}
+			// next line ki prep
+			System.out.println();
+			row++;
+		}
+	}
+
+}
