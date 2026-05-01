a096f72ef330db957b9faf8946cef013b94238ec
monukumar98 <monu012as@gmail.com>
2026-05-02 00:40:04 +0530

    Maths

---
+++ Lec4/Continue_Demo.java
@@ -0,0 +1,19 @@
+package Lec4;
+
+public class Continue_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 8;
+
+		for (int i = 1; i <= 8; i++) {
+			if (i == 6) {
+				continue;
+			}
+			System.out.println(i);
+
+		}
+
+	}
+
+}
