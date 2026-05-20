6ee202520958544d1327bb44ea03be1d86ba0abb
monukumar98 <monu012as@gmail.com>
2026-05-20 21:55:40 +0530

    Sorting Kadanes_Algorithm Moore Voting

---
+++ Lec10/Kadanes_Algorithm.java
@@ -0,0 +1,26 @@
+package Lec10;
+
+public class Kadanes_Algorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(Maximum_Sum(arr));
+	}
+
+	public static int Maximum_Sum(int[] arr) {
+		int ans = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+			ans = Math.max(ans, sum);
+			if(sum<0) {
+				sum=0;
+			}
+
+		}
+		return ans;
+
+	}
+
+}
