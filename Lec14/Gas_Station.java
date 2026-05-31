66ea8f9a90d65d913bf7cecb4ca8bc0499b11b96
monukumar98 <monu012as@gmail.com>
2026-06-01 00:37:02 +0530

    2D Array Questions

---
+++ Lec14/Gas_Station.java
@@ -0,0 +1,19 @@
+package Lec14;
+
+public class Gas_Station {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] gas = { 1, 2, 3, 4, 5 };
+		int[] cost = { 3, 4, 5, 1, 2 };
+	}
+
+	public static int Circuit(int[] gas, int[] cost) {
+		int total_sum=0;
+		for (int i = 0; i < cost.length; i++) {
+			
+		}
+
+	}
+
+}
