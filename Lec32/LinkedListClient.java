ab1a4a83fe90bb5946bdb2dfa8063a4be9bf5275
monukumar98 <monu012as@gmail.com>
2026-07-28 21:15:14 +0530

    LinkedList

---
+++ Lec32/LinkedListClient.java
@@ -0,0 +1,18 @@
+package Lec32;
+
+public class LinkedListClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList();
+		ll.addfirst(10);
+		ll.addfirst(20);
+		ll.addfirst(30);
+		ll.addlast(40);
+		ll.addatindex(9, 2);
+		ll.Display();
+
+	}
+
+}
+
