94819e829094688004199b9edccf4b09705c015d
monukumar98 <monu012as@gmail.com>
2026-08-25 21:08:11 +0530

    HashMap

---
+++ Lec43/HashMapClient.java
@@ -0,0 +1,21 @@
+package Lec43;
+
+public class HashMapClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer> map = new HashMap<>();
+	//	System.out.println(map.hashCode());
+		map.put("Ankit", 77);
+		map.put("Raj", 87);
+		map.put("Shiva", 55);
+		map.put("Ankita", 97);
+		map.put("Dj", 73);
+		map.put("Puneet", 56);
+		map.put("Ankita", 17);
+		System.out.println(map);
+		
+
+	}
+
+}
